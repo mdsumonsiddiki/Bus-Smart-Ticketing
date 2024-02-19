6c3e444b817905d82853257c44f64699d5ead00a
Md Sumon Siddiki <mdsuman.cit.tool@gmail.com>
2024-02-18 20:34:20 -0800

    code mini updated

--- js/script.js
+++ js/script.js
@@ -1,11 +1,10 @@
 const allSeats = document.getElementsByClassName('select-seat');
 
-
 for (const seat of allSeats) {
+
     seat.addEventListener('click', function (e) {
         setColor(e.target);
         disableElement(seat);
-
         // incrementSeat
         let incrementSeat = getInnerText('increment-seat');
         incrementSeat++;
@@ -57,8 +56,9 @@ for (const seat of allSeats) {
                     setInnerTextById('Discount-price', discountPrice)
                     setInnerTextById('grand-price', totalDiscount);
                     addClassById('coupon-area', 'hidden')
+
                 }
-                else if(cupponCodeTwo === offerInputValue) {
+                else if (cupponCodeTwo === offerInputValue) {
                     const discountPriceTwo = totalPrice * 0.20;
                     const totalDiscountTwo = totalPrice - discountPriceTwo;
                     removeClassById('discount', 'hidden')
@@ -66,10 +66,37 @@ for (const seat of allSeats) {
                     setInnerTextById('Discount-price', discountPriceTwo)
                     setInnerTextById('grand-price', totalDiscountTwo);
                     addClassById('coupon-area', 'hidden')
+                    addClassById('coupon-warning', 'hidden')
+                }
+                else {
+                    removeClassById('coupon-warning', 'hidden')
                 }
             })
+        };
+
+        if (seatBook === 1) {
+            removeClassById('submit-btn', 'bg-gray-400')
+            removeClassById('submit-btn', 'pointer-events-none')
+            addClassById('submit-btn', 'bg-[#1DD100]')
         }
 
+        let submitBtn = document.getElementById('submit-btn');
+        submitBtn.addEventListener('click', function (e) {
+            let inputNumber = document.getElementById('number-input').value.trim();
+            let convertInputNumber = parseInt(inputNumber);
+
+            if (convertInputNumber !== "" && !isNaN(convertInputNumber)) {
+                addClassById('input-warning', 'hidden')
+                removeClassById('pop-up', 'hidden')
+                const popUp = document.getElementById('popup-btn');
+                popUp.addEventListener('click', function (e) {
+                    addClassById('pop-up', 'hidden')
+                })
+            }
+            else {
+                removeClassById('input-warning', 'hidden')
+            }
+        })
     })
 }
 
